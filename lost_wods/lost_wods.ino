e238770c7a619eeb2774e911ebc8af07f1fdcb08
Panorama-xd <--global>
2025-02-23 23:30:18 -0600

    Se agrega el proyecto

---
+++ lost_wods/lost_wods.ino
@@ -0,0 +1,54 @@
+int tonePin = 11;
+void setup() {
+
+}
+
+void midi() {
+
+    tone(tonePin, 466, 3.16659367674e+17);
+    delay(3.5184374186e+17);
+    tone(tonePin, 698, 1.05553122558e+17);
+    delay(1.17281247287e+17);
+    tone(tonePin, 659, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 587, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 698, 8.44424980464e+17);
+    delay(9.38249978293e+17);
+    tone(tonePin, 233, 3.16659367674e+17);
+    delay(3.5184374186e+17);
+    tone(tonePin, 349, 1.05553122558e+17);
+    delay(1.17281247287e+17);
+    tone(tonePin, 329, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 293, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 293, 8.44424980464e+17);
+    delay(9.38249978293e+17);
+    tone(tonePin, 466, 3.16659367674e+17);
+    delay(3.5184374186e+17);
+    tone(tonePin, 698, 1.05553122558e+17);
+    delay(1.17281247287e+17);
+    tone(tonePin, 659, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 587, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 698, 8.44424980464e+17);
+    delay(9.38249978293e+17);
+    tone(tonePin, 233, 3.16659367674e+17);
+    delay(3.5184374186e+17);
+    tone(tonePin, 349, 1.05553122558e+17);
+    delay(1.17281247287e+17);
+    tone(tonePin, 329, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 293, 2.11106245116e+17);
+    delay(2.34562494573e+17);
+    tone(tonePin, 293, 8.44424980464e+17);
+    delay(9.38249978293e+17);
+
+}
+
+void loop() {
+    // Play midi
+    midi();
+}
